190bb96ebcfa0e92f655588a665d1a64b4f63ad8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-23 21:14:36 +0000

    stateful widget added

--- lib/main.dart
+++ lib/main.dart
@@ -101,3 +101,15 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
