c186558cc96d156fe3824a8f4a953de578edca19
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-23 17:34:22 +0000

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,33 @@ class NinjaCard extends StatelessWidget {
         backgroundColor: Colors.amber[800],
         elevation: 0.0,
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               'NAME',
               style: TextStyle(
-                color: Colors.black,
+                color: Colors.grey[900],
+                letterSpacing: 2.0,
               ),
-            )
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'a_moah__',
+              style: TextStyle(
+                color: Colors.amber[600],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
           ],
         ),
       ),
