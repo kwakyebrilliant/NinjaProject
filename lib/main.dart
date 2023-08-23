9b96c9b612692cacb21adee806e9ccf32117e0a8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-23 16:59:55 +0000

    padding added

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,19 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: const Text('Ninja ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.amber[800],
-          elevation: 0.0,
-        ));
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.amber[800],
+        elevation: 0.0,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
